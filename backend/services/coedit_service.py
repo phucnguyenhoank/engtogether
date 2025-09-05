6526f6275200b27004a8a8d1859413ef95c3c61c
phucnguyenhoank <phucnguyeho@gmail.com>
2025-09-05 22:22:01 +0700

    auto chunking sentence

--- backend/services/coedit_service.py
+++ backend/services/coedit_service.py
@@ -1,6 +1,18 @@
 from backend.models.coedit_model import CoEdITModel
+import re
 
 _model = CoEdITModel()
 
+def split_to_sentences(text: str) -> list[str]:
+    return re.split(r'(?<=\.)\s+', text)
+
 def process_text(text: str) -> str:
-    return _model.correct_sentence(text)
+    sentences = split_to_sentences(text)
+
+    improved = []
+    for sentence in sentences:
+        if sentence.strip():  # skip empty strings
+            improved.append(_model.correct_sentence(sentence.strip()))
+
+    # join with a space to rebuild the text
+    return " ".join(improved)
--- t.py
+++ t.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, T5ForConditionalGeneration
 
 tokenizer = AutoTokenizer.from_pretrained("grammarly/coedit-large")
 model = T5ForConditionalGeneration.from_pretrained("grammarly/coedit-large")
-input_text = 'Rewrite to make this easier to understand: Today, I worked so hard, but my girlfriend was not understand me, she just blames me that I did not care her.'
+input_text = 'Rewrite to make this easier to understand: Today, I worked so hard, but my girlfriend '
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 outputs = model.generate(input_ids, max_length=256)
 edited_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
